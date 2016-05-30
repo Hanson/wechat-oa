796092b36e566207dc8f27262eccd0b9af0b03bd
hanccc <h@hanc.cc>
2016-05-31 00:49:08 +0800

    bomb

--- tests/test.php
+++ tests/test.php
@@ -9,8 +9,8 @@
 require __DIR__ . './../vendor/autoload.php';
 
 
-$email = 'your email';
-$password = 'your password';
+$email = 'h@hanc.cc';
+$password = 'hanson1994';
 $wechat = \Hanccc\WechatOA::getInstance($email, $password);
 $wechat->sendMessage('hello1', 'oYNFUs5-y5MIxf4m2EQ230-5WRkc', 1882317807);
 $wechat = \Hanccc\WechatOA::getInstance($email, $password);
